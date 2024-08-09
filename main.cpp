77961aec9e12202f91ee389636302d60893ff38e
Windings-Lab <janton20001@gmail.com>
2024-08-09 03:18:02 +0300

    Using SDL callbacks. Deleteing main entry point

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
 #include "SDL3/SDL.h"
 
 constexpr int   WIDTH = 800;
@@ -6,15 +8,13 @@ SDL_Window*     WINDOW = nullptr;
 
 SDL_GLContext   OPENGL_CONTEXT = nullptr;
 
-bool            LOOP_ENABLED = true;
-
-bool InitializeSDL()
+int SDL_AppInit(void **appstate, int argc, char **argv)
 {
     if(SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         SDL_Log("SDL failed to initialize!");
 
-        return false;
+        return SDL_APP_FAILURE;
     }
 
     // Version 4.1
@@ -25,13 +25,13 @@ bool InitializeSDL()
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    
+
     WINDOW = SDL_CreateWindow("Hello SDL and OpenGL", WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
     if(!WINDOW)
     {
         SDL_Log("Window failed to create!");
 
-        return false;
+        return SDL_APP_FAILURE;
     }
 
     // OpenGL Context is a OpenGL state machine
@@ -40,62 +40,35 @@ bool InitializeSDL()
     {
         SDL_Log("OpenGL Context failed to create!");
 
-        return false;
+        return SDL_APP_FAILURE;
     }
 
-    return true;
+    return SDL_APP_CONTINUE;
 }
 
-inline bool IsEventDetected(SDL_Event& e)
+int SDL_AppEvent(void *appstate, const SDL_Event *event)
 {
-    return SDL_PollEvent(&e) == SDL_TRUE;
-}
-
-void Handler()
-{
-    SDL_Event e;
-    
-    while (IsEventDetected(e))
+    switch (event->type)
     {
-        switch (e.type)
-        {
-        case SDL_EVENT_QUIT:
-            SDL_Log("Quiting!");
-            LOOP_ENABLED = false;
-            break;
-        default: ;
-        }
+    case SDL_EVENT_QUIT:
+        SDL_Log("Quiting!");
+        return SDL_APP_SUCCESS;
+    default: ;
     }
+
+    return SDL_APP_CONTINUE;
 }
 
-void Loop()
+int SDL_AppIterate(void *appstate)
 {
-    while (LOOP_ENABLED)
-    {
-        Handler();
-
-        SDL_GL_SwapWindow(WINDOW);
-    }
+    SDL_GL_SwapWindow(WINDOW);
+    return SDL_APP_CONTINUE;
 }
 
-void CleanUp()
+void SDL_AppQuit(void *appstate)
 {
     if(WINDOW)
     {
         SDL_DestroyWindow(WINDOW);
     }
-
-    SDL_Quit();
-}
-
-int main(int argc, char* argv[])
-{    
-    if(InitializeSDL())
-    {
-        Loop();
-    }
-
-    CleanUp();
-    
-    return 0;
 }
