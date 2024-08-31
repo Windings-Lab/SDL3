f27085bdd4d1676da7620cbc6f6f0783ea10bb25
Windings-Lab <janton20001@gmail.com>
2024-08-31 20:24:05 +0300

    GL_Context readablity

--- include/opengl/WD_gl_context.h
+++ include/opengl/WD_gl_context.h
@@ -12,14 +12,15 @@ namespace WD::GL
     class Context
     {
     public:
+        std::vector<std::unique_ptr<class ShaderProgram>> ShaderPrograms;
+
+    public:
         explicit Context(Window* window);
 
         void Iterate();
 
         void CreateShaderProgram();
         void UpdateViewport(int width, int height);
-
-        std::vector<std::unique_ptr<class ShaderProgram>> ShaderPrograms;
 
         ~Context();
 
